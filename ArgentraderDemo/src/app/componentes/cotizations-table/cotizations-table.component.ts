16d22132c72c61832f4cbe9100623f473e5f440b
Bethania La Manna <bethanialamanna@gmail.com>
2023-09-01 12:14:01 -0300

    traigo servicio con array

--- ArgentraderDemo/src/app/componentes/cotizations-table/cotizations-table.component.ts
+++ ArgentraderDemo/src/app/componentes/cotizations-table/cotizations-table.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core';
@@ -8,16 +9,21 @@ import { CotizacionesServiceService } from 'app/services/cotizaciones-service.se
   templateUrl: './cotizations-table.component.html',
   styleUrls: ['./cotizations-table.component.css']
 })
-export class CotizationsTableComponent implements OnInit {
-  // cotizationsData:any;
-  constructor(private cotizacionesService:CotizacionesServiceService){}
-  
 
+// interface CotizationsData {
+//   titulos: any[]; // Ajusta el tipo adecuado para 'titulos'
+// }
+
+export class CotizationsTableComponent implements OnInit {
+  
+  constructor(private cotizacionesService:CotizacionesServiceService){}
+  cotizationsData:any;
   ngOnInit(): void{
   }
   refrescar_cotizaciones(){
-    this.cotizacionesService.getCotizaciones().subscribe(cotizationsData =>{cotizationsData});
-    // console.log(cotizationsData);
+  //this.cotizacionesService.getCotizaciones().subscribe(cotizationsData =>{console.log(cotizationsData)});
+  this.cotizacionesService.getCotizaciones().subscribe(cotizationsData => {this.cotizationsData = cotizationsData["titulos"]});
+  console.log(this.cotizationsData);
   }
 
 }
