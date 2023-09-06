2321b3d5d6f3e3305b16c4f53286ba5f1b699b93
Bethania La Manna <bethanialamanna@gmail.com>
2023-09-05 21:27:32 -0300

    agrego validación en formLogin

--- ArgentraderDemo/src/app/componentes/auth/login-form/login-form.component.ts
+++ ArgentraderDemo/src/app/componentes/auth/login-form/login-form.component.ts
@@ -1,9 +1,30 @@
-import { Component} from '@angular/core';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-empty */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormControl, FormControlName} from '@angular/forms';
+import{Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl:'./login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent  {
-}
+export class LoginFormComponent implements OnInit {
+text:any="";
+flag="";
+validControl=false;
+    loginForm= this.formBuilder.group({
+      email: ['',[Validators.email, Validators.required]],
+      password: ['', Validators.required]
+    });
+    get email() { return this.loginForm.controls.email;}
+    get password() { return this.loginForm.controls.password;}
+  
+    constructor(private formBuilder:FormBuilder){}
+  
+  ngOnInit(): void {
+  }
+  }
