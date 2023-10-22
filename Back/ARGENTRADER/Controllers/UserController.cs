2f02138d64ab1885c9ea46530b9cd4a691dd77f5
Bethabe <bethanialamanna@gmail.com>
2023-10-22 03:35:32 -0300

    organizo proyecto . agrego back

---
+++ Back/ARGENTRADER/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARGENTRADER.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public String Saludar() {
+            return "Hola";
+        }
+
+    }
+}
